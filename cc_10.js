fb1907957c6852ce385ce0c528e0434f3577a6da
tracyn09 <tracyn0099@gmail.com>
2025-02-19 15:10:50 -0500

    Task 2–Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -19,3 +19,23 @@ console.log(prod1.getDetails())
 
 prod1.updateStock(3);
 console.log(prod1.getDetails())
+
+//Task 2
+class Order {
+    constructor(orderId, product, quantity) {
+        this.orderId = orderId
+        this.product = product
+        this.quantity = quantity
+        this.product.updateStock(quantity) //Reduce stock when order is created
+    }
+
+getOrderDetails() {
+    const totalPrice = this.product.price * this.quantity
+    return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${totalPrice} `
+}
+}
+//Test Case
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails())
+
+console.log(prod1.getDetails())
